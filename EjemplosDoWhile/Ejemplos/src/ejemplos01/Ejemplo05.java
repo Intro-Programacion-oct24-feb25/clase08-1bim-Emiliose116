3878d00cc8feafeabd43b158aee82dcb19d8e84f
Emili J Cueva <ecueva611@gmail.com>
2024-11-27 11:51:33 -0500

    Cuarto

--- EjemplosDoWhile/Ejemplos/src/ejemplos01/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos01/Ejemplo05.java
@@ -3,13 +3,13 @@
  */
 package ejemplos01;
 
-/**
- *
- * @author reroes
- */
+import java.util.Locale;
+import java.util.Scanner;
 public class Ejemplo05 {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
         // 2. numero, i[0, n]
         int numero;
         // 3. potencia, i[0, n]
@@ -17,15 +17,22 @@ public class Ejemplo05 {
         // 6. resultado, i[0, n]
         int resultado;
         // numero <-- 4
-        numero = 4;
+        
         // 8. potencia <-- 3
-        potencia = 3;
+        
         // 9. resultado <-- 1
         resultado = 1;
 
-        int contador = 1;
+        int contador = 0;
+        
+        System.out.println("Ingrese el numero a elevar");
+            numero = entrada.nextInt();
+            
+            System.out.println("Ingrese la potencia");
+            potencia = entrada.nextInt();
 
         do {
+           
             resultado = resultado * numero;
             contador = contador + 1;
         } while (contador <= potencia);
