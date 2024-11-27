0457fce57ea0e43e522ae36bc852505b3d639c43
Emili J Cueva <ecueva611@gmail.com>
2024-11-27 12:00:14 -0500

    e

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo06.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo06.java
@@ -32,12 +32,18 @@ public class Ejemplo06 {
         int operacion;
         int contador = 1;
         int limite;
+        String cadena = "";
+        System.out.println("Ingrese la tabla a generar");
+        tabla = entrada.nextInt();
+        
+        System.out.println("Ingrese el limite a generar");
+        limite = entrada.nextInt();
         
         do {
             operacion = tabla * contador;
-            System.out.printf("%d x %d = %d\n", tabla, contador, operacion);
-            contador = contador + 1;
+            cadena = String.format("%s%d x %d = %d\n", cadena, tabla, contador, operacion);
         } while (contador <= limite);
+        System.out.printf("%s", cadena);
 
     }
 }
